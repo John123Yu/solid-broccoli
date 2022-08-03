efd9ad2448a02b1c14bffaeb00d33c2cff6284d1
Yu, John-NONEMP <jyu-NONEMP@Collegeboard.org>
2022-08-03 06:55:03 -0400

    searchbook mutation

--- Develop/client/src/pages/SearchBooks.js
+++ Develop/client/src/pages/SearchBooks.js
@@ -1,11 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
+import { useQuery, useMutation } from '@apollo/client'
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import { SAVE_BOOK } from '../utils/mutations'
+import { QUERY_ME } from "../utils/queries";
 
 const SearchBooks = () => {
+  const { data } = useQuery(QUERY_ME, {})
+  const [saveBook] = useMutation(SAVE_BOOK)
+
+  const user = data?.me || data?.user || {}
   // create state for holding returned google api data
   const [searchedBooks, setSearchedBooks] = useState([]);
   // create state for holding our search field data
@@ -56,25 +63,14 @@ const SearchBooks = () => {
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
-
-    // get token
-    const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-    if (!token) {
-      return false;
-    }
-
     try {
-      const response = await saveBook(bookToSave, token);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      // if book successfully saves to user's account, save book id to state
+      await saveBook({
+        variables: { id: user._id, book: bookToSave }
+      })
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
-    } catch (err) {
-      console.error(err);
+    } catch (e) {
+      console.error(e)
+      throw new Error('something went wrong!');
     }
   };
 
